76d5baf4dceec9f141d3306f065e38b591f7c637
ishida <h_hand_3636@gmail.com>
2020-03-14 00:30:18 +1100

    add time zone

--- Sample.Common/Constant.cs
+++ Sample.Common/Constant.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 namespace Sample.Common
 {
+
     public class Constant
     {
-        public Constant()
-        {
-        }
+        //japanese time zone info
+        readonly TimeZoneInfo japanTimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
+        // eastern
+        readonly TimeZoneInfo etTimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
     }
 }
